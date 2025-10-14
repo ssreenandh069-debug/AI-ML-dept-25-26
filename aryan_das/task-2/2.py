ff84c0d0284c4a314fdf57db510e061d5d9432cd
Aryan Das <aryandas2911@gmail.com>
2025-10-14 23:37:46 +0530

    Added all tasks

---
+++ aryan_das/task-2/2.py
@@ -0,0 +1,16 @@
+import random
+
+characters = ["a sleepy panda", "an alien", "a pirate", "a robot"]
+places = ["in the jungle", "on Mars", "at a tech fest", "in the library"]
+objects = ["a laptop", "a treasure map", "a sandwich", "a phone"]
+actions = ["started coding", "fell asleep", "built a rocket", "lost their WiFi"]
+
+how_many = int(input("How many stories do you want? "))
+
+for i in range(how_many):
+    character = random.choice(characters)
+    place = random.choice(places)
+    obj = random.choice(objects)
+    action = random.choice(actions)
+    
+    print(f"Once upon a time, {character} found {obj} {place} and {action}!")
